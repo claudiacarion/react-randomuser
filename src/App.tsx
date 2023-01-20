7cc2e00419f82cbb822d7f41760e7fe4c1a1ffee
ClaudiaC <claudia.carion@gmail.com>
2023-01-20 16:41:01 +0100

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import UserAddress from './components/UserAddress'
-import UserAge from './components/UserAge'
-import UserName from './components/UserName'
-import UserNameChange from './components/UserNameChange'
-import UserPic from './components/UserPic'
-import { UserData } from './types'
+import React, { useEffect, useState } from 'react';
+import UserAddress from './components/UserAddress';
+import UserAge from './components/UserAge';
+import UserName from './components/UserName';
+import UserNameChange from './components/UserNameChange';
+import UserPic from './components/UserPic';
+import { UserData } from './types';
 
 function App() {
   const [user, setUser] = useState<UserData>({
@@ -12,31 +12,29 @@ function App() {
     age: 0,
     address: '',
     pic: '',
-  })
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    const value = e.currentTarget.value
-    setUser({ ...user, name: value })
-  }
+    e.preventDefault();
+    const { value } = e.currentTarget;
+    setUser({ ...user, name: value });
+  };
 
   useEffect(() => {
     const getData = async () => {
-      const res = await fetch('https://randomuser.me/api/')
-      const data = await res.json()
-      const fetchedUser = data.results[0]
-      setUser(prev => {
-        return {
-          ...prev,
-          name: fetchedUser.name.first,
-          age: fetchedUser.dob.age,
-          address: fetchedUser.location.street.name,
-          pic: fetchedUser.picture.large,
-        }
-      })
-    }
-    getData()
-  }, [])
+      const res = await fetch('https://randomuser.me/api/');
+      const data = await res.json();
+      const fetchedUser = data.results[0];
+      setUser(prev => ({
+        ...prev,
+        name: fetchedUser.name.first,
+        age: fetchedUser.dob.age,
+        address: fetchedUser.location.street.name,
+        pic: fetchedUser.picture.large,
+      }));
+    };
+    getData();
+  }, []);
 
   return (
     <main>
@@ -46,7 +44,7 @@ function App() {
       <UserAddress user={user} />
       <UserNameChange user={user} nameChange={handleChange} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
